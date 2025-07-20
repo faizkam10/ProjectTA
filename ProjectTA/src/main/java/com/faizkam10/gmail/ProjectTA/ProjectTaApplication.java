2c3136eaee3b8bd016850e4bc9e83beefc465230
Muhamad Alfian Widjaya <muhamad.alfian.widjaya@bni.co.id>
2025-07-20 15:01:27 +0700

    stae=rter spring

---
+++ ProjectTA/src/main/java/com/faizkam10/gmail/ProjectTA/ProjectTaApplication.java
@@ -0,0 +1,13 @@
+package com.faizkam10.gmail.ProjectTA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTaApplication.class, args);
+	}
+
+}
---
+++ ProjectTA/src/test/java/com/faizkam10/gmail/ProjectTA/ProjectTaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faizkam10.gmail.ProjectTA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
